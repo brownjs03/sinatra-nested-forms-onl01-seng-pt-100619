7b703a8664b1671da5d35cd3a528a231623744a7
Jeffrey Brown <brownjs03@gmail.com>
2020-02-18 01:33:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
-
+  get '/' do 
+    "Welcome to the Nested Forms Lab!"
+  end 
+  
+  get '/new' do 
+    erb :'pirates/new'
+  end 
+  
+  post '/pirates' do 
+    
+  end 
+  
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,13 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  @@all = [] 
+  
+  
+  
+  def self.clear 
+    @@all.clear 
+  end 
+    
 end
